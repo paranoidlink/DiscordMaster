b3ef3c9b5c4b07b7487ac291ed297f677538e586
Ben <ben.atherton.1998@gmail.com>
2019-10-10 10:13:36 +0100

    1.2.2
    Multiple Dice

--- dm.js
+++ dm.js
@@ -71,9 +71,17 @@ client.on("message", async message => {
         } 
     }
     if (command === "roll"){
-        let [dice, modifier = 0] = args;
-        let result = Math.floor(Math.random()* dice.match(/\d+/)[0]) + parseInt(modifier)
-        message.channel.send(result);
+        let [amount, dice, modifier = 0] = args;
+        let resultArray = []
+        for (i = 0; i < parseInt(amount); i++){
+            resultArray.push(Math.floor(Math.random()* dice.match(/\d+/)[0] +1))
+             }
+             console.log(resultArray);
+             let result = resultArray.reduce((a, b) => a + b)
+             result += parseInt(modifier);
+             message.channel.send(`You rolled: ${resultArray} total roll is: ${result}`);
+           
+        
         }
   });
  
