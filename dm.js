88bf0aed3ed123fad503371d5ec2ec65292d9772
Ben <ben.atherton.1998@gmail.com>
2019-10-10 07:55:44 +0100

    V1.1.0
    Updated to Master Branch of Discord.JS and added new features

--- dm.js
+++ dm.js
@@ -6,23 +6,48 @@ const config = require("./config.json");
 client.on("message", async message => {
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
-   
+    if (!["176476272980000768", "189835256273043456", "151490253692076032"].includes(message.author.id)) return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
     if (command === "new"){
         let [roleName, template, catagoryName, textChannelName, voiceChannelName] = args;
         let templateRole = message.guild.roles.find(role => role.name === template);
-
-        const newRole = await message.guild.createRole({
-        name: roleName,
-        permissions: templateRole.permissions
-    })
+        const colors = [
+            'AQUA',
+            'GREEN',
+            'BLUE',
+            'PURPLE',
+            'LUMINOUS_VIVID_PINK',
+            'GOLD',
+            'ORANGE',
+            'RED',
+            'NAVY',
+            'DARK_AQUA',
+            'DARK_GREEN',
+            'DARK_BLUE',
+            'DARK_PURPLE',
+            'DARK_VIVID_PINK',
+            'DARK_GOLD',
+            'DARK_ORANGE',
+            'DARK_RED',
+            'DARK_NAVY',
+          ]
+        const n = Math.floor(Math.random() *colors.length);
+        const newRole = await message.guild.roles.create({
+            data: {
+                    name: roleName,
+                    permissions: templateRole.permissions,
+                    color : colors[n],
+                    mentionable : templateRole.mentionable,
+                    hoist : templateRole.hoist
+            }
+            })
        .then(role => console.log(`created new role with name ${role.name} and copied permissions from ${template}`))
         .catch(console.error);
         const everyone = message.guild.id;
         let newRoles =  message.guild.roles.find(vrole => vrole.name === roleName);
-        const catagory = await message.guild.createChannel(catagoryName, {type : 'category',
+        const catagory = await message.guild.channels.create(catagoryName, {type : 'category',
         permissionOverwrites : [{
             id: everyone,
             deny : ['VIEW_CHANNEL'],
@@ -33,8 +58,10 @@ client.on("message", async message => {
 
         }]
     })
-        message.guild.createChannel(textChannelName, {type : 'text', parent : catagory.id});
-        message.guild.createChannel(voiceChannelName, {type : 'voice', parent : catagory.id});
+        console.log(newRole);
+        message.guild.channels.create(textChannelName, {type : 'text', parent : catagory.id});
+        message.guild.channels.create(voiceChannelName, {type : 'voice', parent : catagory.id});
+        message.channel.send(`Successfully created ${newRoles.toString()} and the channels to go with it`);
     }
   });
  
