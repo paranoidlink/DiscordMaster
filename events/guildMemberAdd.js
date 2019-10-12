5cc86edc737ff047e0a91c5a686c28c6b7e9811c
Ben <ben.atherton.1998@gmail.com>
2019-10-12 04:52:14 +0100

    1.4.0

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = async (client, member) => {
-    newChannel = await member.guild.channels.create(member.user.username + "s-dm-secrets", {type : 'text', parent : '632418237237166098'});
+    newChannel = await member.guild.channels.create(member.user.username + "s-dm-secrets", {type : 'text', parent : '612980624692609044'});
     newChannel.send(`Hey ${member.toString()} this channel is your direct line of communication with DM's`);
 
 }
