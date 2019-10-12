c8e23d0d96ce4bf4594a3417d9168ef60fa719ad
Ben <ben.atherton.1998@gmail.com>
2019-10-12 05:10:38 +0100

    1.4.1
    Added permissions to DM Secrets

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,18 @@
 module.exports = async (client, member) => {
-    newChannel = await member.guild.channels.create(member.user.username + "s-dm-secrets", {type : 'text', parent : '612980624692609044'});
+    const everyone = member.guild.id
+    newChannel = await member.guild.channels.create(member.user.username + "s-dm-secrets", {type : 'text', parent : '612980624692609044', permissionOverwrites : [{
+        id : everyone,
+        deny : ['VIEW_CHANNEL'],
+    },
+    {
+        id : member.id,
+        allow : ['VIEW_CHANNEL'],
+    },
+    {
+        id : '612954822907396096',
+        allow : ['VIEW_CHANNEL']
+    }]
+    });
     newChannel.send(`Hey ${member.toString()} this channel is your direct line of communication with DM's`);
 
 }
